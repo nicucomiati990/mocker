1a5345278b228d312da3f9eeb7d13d621f443326
nicucomiati <nicu.comiati@gmail.com>
2019-09-21 12:49:53 +0300

    commit before eject

--- src/App.js
+++ src/App.js
@@ -1,55 +1,13 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import React from 'react';
+import MockView from './mock-view/mock-view';
 
 const App = () => {
-  const [text, setText] = useState("");
-  const [mockedText, setMockedText] = useState("");
-
-  const mock = () => {
-    const letters = text.split("");
-    let result = "";
-    for (const letter of letters) {
-        if (Math.random() < 0.5) {
-            result += letter.toUpperCase();
-        } else {
-            result += letter;
-        }
-    }
-    setMockedText(result);
-}
-
-  return (
-    <div className="App">
-      <h1>MoCkEr</h1>
-      <div>
-        <input 
-          value={text}
-          maxLength={30}
-          onChange={(ev) => {
-            setText(ev.target.value);
-            setMockedText("");
-          }}
-          className="mock-input" 
-          placeholder="Your text goes here"
-        />
-        <button 
-          className="mock-button"
-          onClick={mock}
-        >
-          mOcK
-        </button>
-      
-      </div>
-      <h2 className="mocked-text">
-        {mockedText}
-      </h2>
-      {mockedText.length
-        ? <button className="reset-mock" onClick={() => {setMockedText(""); setText("");}}>Reset</button>
-        : null
-      }
-    </div>
-  );
-}
+	return (
+		<div className="App">
+			<MockView />
+		</div>
+	);
+};
 
 export default App;
