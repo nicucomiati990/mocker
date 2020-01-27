da0a8a981bbdbf745afedcb8dd729b537301a6c7
nicucomiati990 <nicu.comiati@gmail.com>
2020-01-27 17:19:06 +0200

    changed styling

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import styles from './chat.module.scss';
-import userIcon from './user.png';
+import React from "react";
+import styles from "./chat.module.scss";
+import userIcon from "./user.png";
 
-const UserIcon = () => <img
-    className={styles.userIcon}
-    height={32}
-    width={32}
-    src={userIcon} alt="User"
-/>
+const UserIcon = () => (
+    <img
+        className={styles.userIcon}
+        height={32}
+        width={32}
+        src={userIcon}
+        alt="User"
+    />
+);
 
 const Chat = ({ text, mockedText, isVisible }) => {
     if (!isVisible) return null;
@@ -16,14 +19,12 @@ const Chat = ({ text, mockedText, isVisible }) => {
         <div className={styles.chatPanel}>
             <div className={styles.chat}>
                 <UserIcon />
-                <div className={styles.textChat}>
-                    {text}
-                </div>
+                <div className={styles.textChat}>{text}</div>
             </div>
 
             <div className={styles.mockedTextChat}>{mockedText}</div>
         </div>
     );
-}
+};
 
 export default Chat;
--- src/mock-view/mock-view.js
+++ src/mock-view/mock-view.js
@@ -1,74 +1,83 @@
-import React, { useState } from 'react';
-import styles from './mock-view.module.scss';
-import { mockText } from '../utils/mock-text';
-import Chat from '../chat/chat';
-import toastr from 'toastr';
-import 'toastr/build/toastr.min.css';
+import React, { useState } from "react";
+import Chat from "../chat/chat";
+import toastr from "toastr";
+import { mockText } from "../utils/mock-text";
+import "toastr/build/toastr.min.css";
+import styles from "./mock-view.module.scss";
 
 const MockView = () => {
-	const [text, setText] = useState('');
-	const [mockedText, setMockedText] = useState('');
-	const [isMockPanelVisible, setMockPanelVisible] = useState(false);
+    const [text, setText] = useState(
+        "safdafsafsdafdsafsdagsagfsaifsidfbisdafisnfsafsafdafnsdafbusdabfubasfusafdfnsdaifndsaifnsiaufniusnf"
+    );
+    const [mockedText, setMockedText] = useState("");
+    const [isMockPanelVisible, setMockPanelVisible] = useState(false);
 
-	const mock = () => {
-		if (text) {
-			setMockedText(mockText(text));
-			setMockPanelVisible(true);
+    const mock = () => {
+        if (text) {
+            setMockedText(mockText(text));
+            setMockPanelVisible(true);
+        } else {
+            toastr["error"]("PlEasE pRoViDe tExT tO mOcK", "ErRoR");
+        }
+    };
 
-		} else {
-			toastr["error"]("PlEasE pRoViDe tExT tO mOcK", "ErRoR");
-		}
-	};
+    const mockButtonText = mockedText.length ? "mOcK AgAiN" : "mOcK";
 
-	const mockButtonText = mockedText.length ? "mOcK AgAiN" : "mOcK";
+    const resetMockButton = isMockPanelVisible ? (
+        <button
+            className={styles.resetMock}
+            onClick={() => {
+                setMockPanelVisible(false);
+                setText("");
+                setMockedText("");
+            }}
+        >
+            Reset
+        </button>
+    ) : null;
 
-	const resetMockButton = isMockPanelVisible
-		? <button
-			className={styles.resetMock}
-			onClick={() => {
-				setMockPanelVisible(false);
-				setText('');
-				setMockedText('');
-			}}>Reset</button>
-		: null;
-
-	return (
-		<>
-			<h1 className={styles.header}>MoCkEr</h1>
-			<div className={styles.subheader}>Mock your imaginary friends</div>
-			<div className={styles.footer}>
-				Made by Nicu Comiati
-				<a
-					rel="noopener noreferrer"
-					target="_blank"
-					href="https://github.com/nicucomiati990/mocker">
-					See on GitHub
-					</a>
-			</div>
-			<div className={styles.mockFields}>
-				<textarea
-					rows={3}
-					value={text}
-					maxLength={30}
-					onChange={(ev) => {
-						setText(ev.target.value);
-						setMockPanelVisible(false);
-					}}
-					className={styles.mockInput}
-					placeholder="Your text goes here"
-				/>
-				<button className={styles.mockButton} onClick={mock}>
-					{mockButtonText}
-				</button>
-			</div>
-			{resetMockButton}
-			<Chat
-				isVisible={isMockPanelVisible}
-				text={text}
-				mockedText={mockedText}
-			/>
-		</>
-	);
+    return (
+        <div className={styles.mockView}>
+            <div>
+                <h1 className={styles.header}>MoCkEr</h1>
+                <div className={styles.subheader}>
+                    Mock your imaginary friends
+                </div>
+            </div>
+            <div className={styles.mockFields}>
+                <textarea
+                    rows={6}
+                    value={text}
+                    maxLength={300}
+                    onChange={ev => {
+                        setText(ev.target.value);
+                        setMockPanelVisible(false);
+                    }}
+                    className={styles.mockInput}
+                    placeholder="Your text goes here"
+                />
+                <button className={styles.mockButton} onClick={mock}>
+                    {mockButtonText}
+                </button>
+                {resetMockButton}
+            </div>
+            <Chat
+                isVisible={isMockPanelVisible}
+                text={text}
+                mockedText={mockedText}
+            />
+            <div className={styles.footer}>
+                Made by Nicu Comiati
+                <a
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href="https://github.com/nicucomiati990/mocker"
+                >
+                    See on GitHub
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default MockView;
